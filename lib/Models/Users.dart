b2e9f5bca863626c5ad1ecf4c073e207579f395f
SerdarAtesc <50891117+SerdarAtesc@users.noreply.github.com>
2021-02-06 22:27:23 +0300

    Auth Service and Auth Model

---
+++ lib/Models/Users.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Users {
+  final String id;
+  final String kullaniciAdi;
+  final String fotourl;
+  final String email;
+  final String hakkinda;
+
+  Users({this.id, this.kullaniciAdi, this.fotourl, this.email, this.hakkinda});
+
+  factory Users.productFromFirebase(User users) {
+    return Users(
+      id: users.uid,
+      kullaniciAdi: users.displayName,
+      fotourl: users.photoURL,
+      email: users.email,
+    );
+  }
+
+  factory Users.dokumandanuret(DocumentSnapshot doc) {
+    return Users(
+      id: doc.id,
+      kullaniciAdi: doc.data()['kullaniciAdi'],
+      email: doc.data()['email'],
+      fotourl: doc.data()['fotoUrl'],
+      hakkinda: doc.data()['hakkinda'],
+    );
+  }
+}
---
+++ lib/Services/AuthService.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:preservingculturalheritage/Models/Users.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Auth {
+  final FirebaseAuth auth;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  Auth({this.auth});
+  Stream<User> get user => auth.authStateChanges();
+
+  Users _userRegister(User user) {
+    return user == null ? null : Users.productFromFirebase(user);
+  }
+
+  Stream<Users> get stateTracker {
+    return _firebaseAuth.authStateChanges().map(_userRegister);
+  }
+
+  Future<Users> _userRegisterWithMail(String email, String password) async {
+    var registerCard = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    return _userRegister(registerCard.user);
+  }
+
+  Future<Users> _userLoginWithMail(String email, String password) async {
+    var loginCard = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return _userRegister(loginCard.user);
+  }
+
+  Future<void> _logOut() {
+    return _firebaseAuth.signOut();
+  }
+
+  Future<Users> _loginWithGoogle() async {
+    GoogleSignInAccount googleAccount = await GoogleSignIn().signIn();
+    GoogleSignInAuthentication googleAuth = await googleAccount.authentication;
+    AuthCredential loginWithoutPassword = GoogleAuthProvider.credential(
+      idToken: googleAuth.idToken,
+      accessToken: googleAuth.accessToken,
+    );
+    UserCredential loginCard =
+        await _firebaseAuth.signInWithCredential(loginWithoutPassword);
+    return _userRegister(loginCard.user);
+  }
+
+  Future<void> resetPassword(String email) async {
+    await _firebaseAuth.sendPasswordResetEmail(email: email);
+  }
+}
