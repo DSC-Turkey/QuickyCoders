76c10aa622bea54ac2f9eda04c9d2dcca7309d0c
SerdarAtesc <50891117+SerdarAtesc@users.noreply.github.com>
2021-02-06 23:43:03 +0300

    Firebase Service

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:preservingculturalheritage/sayfalar/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(new MyApp());
+import 'package:flutter/material.dart';
+import 'package:preservingculturalheritage/Pages/authPage.dart';
+import 'package:preservingculturalheritage/Services/AuthService.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Culture App',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new LoginPage(),
-    );
+    return Provider<AuthService>(
+        create: (_) => AuthService(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Culture App',
+          theme: new ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: AuthPage(),
+        ));
   }
 }
